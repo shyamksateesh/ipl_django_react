f8996aa5f3646d3cb583f6bbb0c4733c50c6b0df
Shyam Krishna <shyam@Shyams-MacBook-Pro.local>
2024-04-09 11:03:13 +0530

    initial navbar

--- cricket_frontend/src/App.js
+++ cricket_frontend/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import './App.css';
-import TopBowlers from './components/TopBowlers'; // Import TopBowlers component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar'; 
+import MatchWinLose from './components/MatchWinLose'; 
+import TopBowlers from './components/TopBowlers'; 
 import MatchStatsGraph from './components/MatchStatsGraph';
 import StackedBarChart from './components/StackedBarChart';
-import MatchWinLose from './components/MatchWinLose';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <MatchWinLose />
-      <TopBowlers /> 
-      <MatchStatsGraph />
-      <StackedBarChart />
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<div>
+          <MatchStatsGraph />
+          <StackedBarChart />
+        </div>} />
+        <Route path="/match-win-lose" element={<MatchWinLose/>} />
+        <Route path="/top-bowlers" element={<TopBowlers/>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
---
+++ cricket_frontend/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/match-win-lose">Match Win/Lose</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/top-bowlers">Top Bowlers</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
